d07330977e01032ead8247db277a1b610942d7a5
Finashyn Yaroslav <finashyn.y@gmail.com>
2023-09-17 14:52:37 +0300

    Samurai-way 1-12

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/header/Header";
+import {Navbar} from "./components/navbar/Navbar";
+import {Profile} from "./components/profile/Profile";
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
 }
+
 
 export default App;
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+};
